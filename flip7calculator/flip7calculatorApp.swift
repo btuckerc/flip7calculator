33f833c527b4722296f3870bf01c476b28261bc7
btuckerc <35579682+btuckerc@users.noreply.github.com>
2025-12-16 18:54:25 -0500

    Initial Commit

---
+++ flip7calculator/flip7calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  flip7calculatorApp.swift
+//  flip7calculator
+//
+//  Created by Tucker Craig on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct flip7calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flip7calculatorTests/flip7calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  flip7calculatorTests.swift
+//  flip7calculatorTests
+//
+//  Created by Tucker Craig on 12/16/25.
+//
+
+import Testing
+@testable import flip7calculator
+
+struct flip7calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
